c688ef923374d2d7c9f935b5887f3fbd906242aa
Carlos Hurtado <76065731+churta2@users.noreply.github.com>
2022-07-02 15:09:39 -0400

    Add files via upload
    Decision support systems for 2-8 arms

---
+++ 8_arms.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+from scipy.spatial import distance
+
+def choosing_direction(nn,L,H,t1,t2):
+    
+    file_posterior='input'+ '_'+str(int(nn))+'.csv'
+    data_historical=pd.read_csv(file_posterior, header=None)  
+    set_observations=data_historical.to_numpy()
+    
+    x1=set_observations[-1,0]
+    x2=set_observations[-1,1]
+    
+    lgth=168
+    
+    
+    comparison_table=np.array([[x1 +lgth,x2,0 ],
+                                [x1 +lgth*np.cos(np.pi*0.25), x2+lgth*np.sin(np.pi*0.25),0],
+                                [x1, x2 +lgth, 0],
+                                [x1-lgth*np.cos(np.pi*0.25), x2+lgth*np.cos(np.pi*0.25),0],
+                                [x1-lgth, x2,0],
+                                [x1-lgth*np.cos(np.pi*0.25), x2-lgth*np.cos(np.pi*0.25),0],
+                                [x1, x2-lgth, 0],
+                                [x1+lgth*np.cos(np.pi*0.25), x2-lgth*np.cos(np.pi*0.25),0]])
+    
+    
+    
+    
+    
+    for i in range(8):
+        
+        comparison_table[i,2]=distance.euclidean((comparison_table[i,0],comparison_table[i,1]),(t1,t2))
+    
+    
+    which_dir=np.argmin(comparison_table[:,2])
+    
+    return which_dir+1
+        
+
+theta=choosing_direction(nn,L,H,t1,t2) 
+theta = float(theta)
+   
