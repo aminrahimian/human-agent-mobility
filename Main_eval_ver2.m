c44d681bf6d09a0299977375eee093e04714c35b
Xinyu Si <xy.si@pitt.edu>
2022-11-17 16:03:19 -0500

    Add two new versions of code. ver2: multi-sample; ver2_par: multi-sample with parallel computation

---
+++ Main_eval_ver2.m
@@ -0,0 +1,228 @@
+% This file evaluates different python algorithoms under the same
+% rule.
+
+% To do: enable parfor
+
+clear;
+addpath('SubFunc');
+load("Real_Targets.mat");
+
+% set the environmental condition
+Dim1 = 2000;
+Dim2 = 2000;
+dx = Dim1/200;
+x1 = 0:dx:Dim1;
+x2 = 0:dx:Dim2;
+[X,Y] = meshgrid(x1,x2);
+board = [0,0;Dim1,0;Dim1,Dim2;0,Dim2;0,0];
+
+% agent parameters
+V_best = 2.8; % the desired moving velocity under the best environmental condition
+tau = 0.5; % relaxation time
+dt = tau; % time for velocity adjustment
+dT = 60; % time for movement after velocity adjustment
+Epsilon_prime = 100; % detection radius parameter
+Epsilon = Epsilon_prime;
+radius_factor_best = 2; % The radius factor of receiving signal under best env condition
+% Phi = radius_factor * Epsilon
+
+N_arm = 8; % number of arms used
+theta_list = linspace(0,2*(1-1/N_arm),N_arm) * pi; % the space of moving directions
+
+% sampling parameter
+N_sample = 10; % the number of samples taken by agent at each step
+
+
+% determine whether add environmental condition
+ENV = 0; % if 1, heterogeneous environmental condition is used
+if ENV == 1
+    load('roi.mat');
+    inroi = inROI(roi1,X(:),Y(:)) | inROI(roi2,X(:),Y(:));
+    inroi = reshape(inroi,length(x1),length(x2));
+else
+    inroi = false(length(x1),length(x2));
+end
+scale_factor = ones(length(x1),length(x2));
+scale_factor(inroi) = 0.3; % change this for scaling of the agent's movement
+
+% set total number of loops
+NN = 5;
+TimeLength_record = zeros(NN,1); % record the leng of time needed
+Regret_record = cell(NN,1);
+
+% The Main Loop
+for nn = 1:NN
+
+    % display the number of realization
+    disp(num2str(nn));
+
+    % delete all files from past loop
+    delete input_*.csv
+
+    % randomly generate the location of the real target
+    Real_Target = [Real_Target_All(nn,1) * Dim1, Real_Target_All(nn,2) * Dim2];
+
+    % initial location
+    xy_pede = [0,0]; % initial location
+    xy_pede_save = xy_pede; % save all location information
+
+    % initial moving direction
+    v_desire = V_best * interp2(X,Y,scale_factor,xy_pede(1),xy_pede(2),'nearest',1); % desired velocity [dependent on environment]
+    vel_pede = v_desire; % initial velocity
+    [theta_pede,idx] = datasample(theta_list,1); % randomly sample a initial direction
+    u_pede = vel_pede.*cos(theta_pede); % u, v component of pedestrian
+    v_pede = vel_pede.*sin(theta_pede);
+    uv_pede = [u_pede, v_pede]; % initial velocity
+
+    % probability of receiving a signal
+    radius_factor = radius_factor_best * interp2(X,Y,scale_factor,xy_pede(1),xy_pede(2),'nearest',1);
+    Likely = exp(-1*norm(xy_pede - Real_Target)./(radius_factor * Epsilon_prime));
+
+    % output map to feed to python code
+    output_mat = [];
+    output_mat = [ones(N_sample,2).*xy_pede, binornd(1,Likely,[N_sample,1])];
+
+    % initial time index
+    t = 1;
+
+    % write output matrix to .csv file
+    writematrix(output_mat,['input_',num2str(t),'.csv']);
+
+    % initial some variables to record value
+    T_total = 0;
+    Regret = [];
+
+    % update the result using the output from python
+    while norm(xy_pede - Real_Target) >= Epsilon
+
+        % if don't want to display
+%         disp([num2str(T_total/3600),' hours']);
+
+        % use pyrunfile
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %%%%%%% Edit here according to different python core %%%%%%%
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+        idx = pyrunfile('8_arms.py', 'theta',...
+                        nn = t,...
+                        L = Dim1,...
+                        H = Dim2,...
+                        t1 = Real_Target(1,1),...
+                        t2 = Real_Target(1,2));
+
+%         theta_target = theta_list(double(idx{1}));
+
+        idx = cell(idx);
+        theta_target = theta_list(double(idx{1}));
+        Regret(1,t) = double(idx{2});
+
+        % setup the location of the fake target according to the command
+        l_target = 1000;
+        r_target_x = xy_pede(1) + l_target .* cos(theta_target);
+        r_target_y = xy_pede(2) + l_target .* sin(theta_target);
+        r_target = [r_target_x, r_target_y];
+
+        % update the movement and location of the agent
+        [F_Di]=calc_F_Di(xy_pede,uv_pede,r_target,v_desire,tau);
+        uv_pede_old = uv_pede; % save the previous step velocity
+        uv_pede = uv_pede + F_Di*dt; % update velocity
+        xy_pede = xy_pede + uv_pede*dT;
+
+        % apply reflective boundary condition
+        if ~inpolygon(xy_pede(1),xy_pede(2),board(:,1),board(:,2))
+            xy_pede(xy_pede < 0) = abs(xy_pede(xy_pede < 0));
+            if xy_pede(1) > Dim1
+                xy_pede(1) = 2*Dim1 - xy_pede(1);
+            end
+            if xy_pede(2) > Dim2
+                xy_pede(2) = 2*Dim2 - xy_pede(2);
+            end
+        end
+
+        % update the moving velocity according to the env condition
+        v_desire = V_best * interp2(X,Y,scale_factor,xy_pede(1),xy_pede(2),'nearest',1);
+
+        % updata time step
+        t = t+1;
+        
+        % record the full moving history for this realization
+        xy_pede_save(t,:) = xy_pede;
+
+        % update the likelyhood equation
+        radius_factor = radius_factor_best * interp2(X,Y,scale_factor,xy_pede(1),xy_pede(2),'nearest',1);
+        Likely = exp(-1*norm(xy_pede - Real_Target)./(radius_factor * Epsilon_prime));
+
+        % output map to feed to python code, each row
+%         output_mat(t,1:2) = xy_pede;
+%         output_mat(t,3) = binornd(1,Likely);
+        temp_a = [ones(N_sample,2).*xy_pede, binornd(1,Likely,[N_sample,1])];
+        output_mat = [output_mat;temp_a];
+
+        % judge if pass the target during each time step
+        v1 = xy_pede_save(t,:); % the first vertex
+        v2 = xy_pede_save(t-1,:); % the second vertex
+        vec1 = v2 - v1;
+        vec2 = Real_Target - v1;
+        if dot(vec1,vec2)>=0 && dot(vec1,vec2)<=dot(vec1,vec1)
+            d = abs( det([Real_Target-v1;v2-v1]) )/norm(v2-v1); % distanct from the real target to v1-v2
+            if d <= Epsilon
+                break
+            end
+        end
+        
+        % delete old .csv file
+        % write updated output matrix to .csv file
+        delete input_*.csv
+        writematrix(output_mat,['input_',num2str(t),'.csv']);
+        
+        % set the upper breaking time limit
+        T_total = T_total + dT;
+        if T_total/3600> 72
+            break
+        end
+    end
+
+    % Record the total time length needed for this realization
+    TimeLength_record(nn) = T_total/3600;
+    Regret_record{nn} = Regret;
+    disp(['Realization ',num2str(nn),' using ',num2str(T_total/3600),' hours']);
+end
+
+save('8arms_regret.mat','Regret_record','-v7.3');
+writematrix(TimeLength_record,'8arms_timelength.csv');
+
+
+%% Plotting a single track
+figure(2);
+hold on
+plot(Real_Target(1),Real_Target(2),'rp','markersize',10,'MarkerFaceColor','r');
+plot(0,0,'gs','markersize',10,'MarkerFaceColor','g');
+plot(xy_pede_save(:,1),xy_pede_save(:,2),'m-','LineWidth',1.5);
+plot(xy_pede_save(end,1),xy_pede_save(end,2),'bo','markersize',10,'MarkerFaceColor','b');
+if ENV
+    load('roi.mat');
+    x1 = roi1.Position;
+    x2 = roi2.Position;
+    fill(x1(:,1),x1(:,2),'b', 'FaceAlpha', 0.3,'edgecolor','none');
+    fill(x2(:,1),x2(:,2),'b', 'FaceAlpha', 0.3,'edgecolor','none');
+end
+axis([0 Dim1 0 Dim2]);
+% set(gca,'Ydir','reverse');
+xlb = xlabel('m');
+ylb = ylabel('m');
+ttl = title('Random Walk');
+if ENV
+    lgd = legend('real target','agent initial location','agent path','agent current position','environmental condition',...
+        'location','northeast');
+else
+    lgd = legend('real target','agent initial location','agent path','agent current position',...
+        'location','northwest');
+end
+legend boxoff
+set(gca,'TickLabelInterpreter','latex','fontsize',10);
+set([xlb,ylb,ttl,lgd],'interpreter','Latex','fontsize',12);
+
+box on
+set(gcf,'units','pixels','innerposition',[200,200,500,500]);
+set(gca,'looseInset',[0 0 0 0]);
+
