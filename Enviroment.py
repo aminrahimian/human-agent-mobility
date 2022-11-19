09e326ba09e3a45fe98a1442ccbe719f313d7244
carloshurtado <CAH259@pitt.edu>
2022-11-19 15:29:28 -0500

    Add Class Enviroment Add method to initialize enviroment and to generate signals

--- Enviroment.py
+++ Enviroment.py
@@ -1,3 +1,22 @@
 import pandas as pd
+import numpy as np
+from scipy.spatial import distance
+from scipy.stats import bernoulli
 
-## code to model the enviroment
+
+class Enviroment:
+
+    def __init__(self,L,target_x,target_y,d):
+
+        self.lenght=L
+        self.target_x=target_x
+        self.target_y=target_y
+        self.device_factor=d
+
+
+    def generate_signal(self, pos_x, pos_y):
+
+        l_between_point=distance.euclidean((pos_x,pos_y),(self.target_x,self.target_y))
+        likelihood= np.exp(-l_between_point/self.device_factor)
+
+        return bernoulli.rvs(likelihood, size=1)[0]
