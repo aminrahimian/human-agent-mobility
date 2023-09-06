Add path_x and path_y attributes to agent class to record the search path. Fix the calculation of the probability of choosing parameter mu

actor_critic_episodic.py
@@ -133,34 +133,38 @@ class Agent:
         self.center_lay1 = list(itertools.product(grid_line, grid_line))
         self.center_lay2 = list(itertools.product(grid_line, grid_line))
         self.center_init = [(L/2, L/2)]
-        self.centers = self.center_init + self.center_lay1 + self.enter_lay2
-        self.weigths_vector = [100] + len(self.center_lay1) * [100] + len(self.center_lay2) * [0]
-        self.theta_mu_x_vector=  [1] + len(self.center_lay1) * [1] + len(self.center_lay2) * [0]
-        self.theta_sd_x_vector = [1] + len(self.center_lay1) * [1] + len(self.center_lay2) * [0]
-        self.theta_mu_y_vector = [1] + len(self.center_lay1) * [1] + len(self.center_lay2) * [0]
-        self.theta_sd_y_vector = [1] + len(self.center_lay1) * [1] + len(self.center_lay2) * [0]
+        self.centers = self.center_init + self.center_lay1 + self.center_lay2
+        self.weigths_vector = np.array([100] + len(self.center_lay1) * [100] + len(self.center_lay2) * [0])
+        self.theta_mu_x_vector=  [0.1] + len(self.center_lay1) * [0.1] + len(self.center_lay2) * [0]
+        self.theta_sd_x_vector = [0.01] + len(self.center_lay1) * [0.01] + len(self.center_lay2) * [0]
+        self.theta_mu_y_vector = [0.01] + len(self.center_lay1) * [0.01] + len(self.center_lay2) * [0]
+        self.theta_sd_y_vector = [0.01] + len(self.center_lay1) * [0.01] + len(self.center_lay2) * [0]
         self.theta=self.theta_mu_x_vector+self.theta_mu_x_vector
         self.radius_coarse = radius_coarse
         self.radius_detection=radius_detection
         self.freq_sampling=freq_sampling
         self.cum_targets = 0
-        self.alpha = 0.9
+        self.alpha = 0.1
         self.gamma = 1
         self.time_step = 0
         self.episodes=episodes
         self.parameter=2
+        self.episode=0
+        self.path_x=[self.pos_x]
+        self.path_y=[self.pos_y]
 
+    def feature_vector(self, pos_x, pos_y):
 
-    def feature_vector(self, pos_x, pos_y, episode):
-
-        if episode==0:
+        if self.episode==0:
 
             inside_circle = np.array([1] + len(self.center_lay1) * [0] + len(self.center_lay2) * [0])
+            # print(" init state")
 
-        elif (episode>0) & (episode<self.episodes):
+        elif (self.episode>0) & (self.episode<self.episodes):
 
+            # print(">1")
             actual_pos = len(self.center_lay1) * [(pos_x, pos_y)]
-            iter = len(self.centers)
+            iter = len(self.center_lay1)
             distances = np.array([distance.euclidean(self.center_lay1[i], actual_pos[i]) for i in range(iter)])
             inside_circle_1 = (distances <= self.radius_coarse)
             inside_circle_1 = list(inside_circle_1.astype(int))
@@ -170,8 +174,9 @@ class Agent:
 
         else:
 
+            print("Final grid ")
             actual_pos = len(self.center_lay1) * [(pos_x, pos_y)]
-            iter = len(self.centers)
+            iter = len(self.center_lay1)
             distances = np.array([distance.euclidean(self.center_lay1[i], actual_pos[i]) for i in range(iter)])
             inside_circle_1 = (distances <= self.radius_coarse)
             inside_circle_1 = list(inside_circle_1.astype(int))
@@ -179,7 +184,7 @@ class Agent:
             inside_circle = [0] + len(self.center_lay1) * [0]+inside_circle_1
 
 
-        return inside_circle
+        return np.array(inside_circle)
 
     def sample_action_continous(self):
 
@@ -210,26 +215,25 @@ class Agent:
         x_sa_mu_3 =  zeros_x_sa + partial_x_sa
         h_mu_2 = np.dot(self.theta, x_sa_mu_2)
         h_mu_3 = np.dot(self.theta, x_sa_mu_3)
-        p_mu_2 = h_mu_2/(h_mu_2+h_mu_3)
+        p_mu_2 = np.exp(h_mu_2)/(np.exp(h_mu_2) +np.exp(h_mu_3))
+
+        print("Probability of u_2 : " +str(p_mu_2))
 
         is_mu_2=bernoulli.rvs(p_mu_2, size=1)[0]
 
         if is_mu_2==1:
 
-            parameter=2
+            self.parameter=2
 
         else:
 
-            parameter=3
-
-        self.parameter=parameter
-
-        return parameter
+            self.parameter=3
 
     def sample_action(self):
 
         self.sample_parameter()
-        mu=self.parameter
+        mu=self.parameter-1
+        print("parameter mu :" +str(self.parameter))
         l=pareto.rvs(mu, scale=500,size=1)[0]
         angle=np.random.uniform(0,2*np.pi,1)[0]
 
@@ -248,8 +252,10 @@ class Agent:
         h_mu_2 = np.dot(self.theta, x_sa_mu_2)
         h_mu_3 = np.dot(self.theta, x_sa_mu_3)
 
-        p_mu_2 = h_mu_2 / (h_mu_2 + h_mu_3)
-        p_mu_3 = h_mu_3 / (h_mu_2 + h_mu_3)
+        p_mu_2 = np.exp(h_mu_2) / (np.exp(h_mu_2) + np.exp(h_mu_3))
+        p_mu_3 = 1- p_mu_2
+
+        print("p_mu_2, p_mu_3: " + str((p_mu_2, p_mu_3)))
 
         cum_soft_max=p_mu_2*np.array(x_sa_mu_2)+\
                      p_mu_3*np.array(x_sa_mu_3)
@@ -261,7 +267,6 @@ class Agent:
         else:
 
             nabla = x_sa_mu_3 - cum_soft_max
-
 
         return  nabla
 
@@ -288,6 +293,9 @@ class Agent:
 
         self.pos_x = self.pos_x_prime
         self.pos_y = self.pos_y_prime
+        self.path_x.append(self.pos_x)
+        self.path_y.append(self.pos_y)
+        self.episode+=1
 
     def updates_weigts(self, reward, action):
 
@@ -300,8 +308,8 @@ class Agent:
         current_s = np.dot(self.feature_vector(self.pos_x, self.pos_y), self.weigths_vector)
         next_s = self.gamma * np.dot(self.feature_vector(self.pos_x_prime, self.pos_y_prime), self.weigths_vector)
         delta = reward +next_s-current_s
-        self.weigths_vector=self.weigths_vector+\
-                            self.alpha*delta*self.feature_vector(self.pos_x, self.pos_y)
+        self.weigths_vector=np.array(self.weigths_vector)+\
+                            np.array(self.alpha*delta*self.feature_vector(self.pos_x, self.pos_y))
 
         self.theta = self.theta+\
             self.alpha*delta*self.nabla_pi_sa()
@@ -312,22 +320,32 @@ class Agent:
 
 
 
-
-
 radius_coarse=750
 L=10000
 T=50
 radius_detection=25
 freq_sampling=1
-episodes=50
+episodes=5
 
 a1=Agent(radius_coarse,L,T,radius_detection, freq_sampling, episodes)
 
-action= a1.sample_action()
 
-a1.updates_weigts(24, action)
+for i in range(6):
 
-print(a1.pos_y)
+    print("==============================")
+    print("Episode: " + str(a1.episode))
+    action = a1.sample_action()
+    print("action " +str (action))
+    reward=20
+    a1.updates_weigts(reward, action)
+    print("x: " + str(a1.pos_x) + ",y:"  +str(a1.pos_y))
+    time.sleep(1)
+
+
+print(a1.path_x)
+print(a1.path_y)
+
+
 
 
 