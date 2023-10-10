385a7404226ca113f1c6134c250ee594a590aa00
LoneStar97 <111799839+LoneStar97@users.noreply.github.com>
2023-10-10 11:27:37 -0400

    Add files via upload

---
+++ Varied_mu_search/Targets.m
@@ -0,0 +1,37 @@
+% generate target distribution
+clear;clc;
+
+mean = 5000;
+std_dev = 3000;
+num_patches = 10;
+
+x_patch = mean + std_dev * randn(num_patches, 1);
+y_patch = mean + std_dev * randn(num_patches, 1);
+
+scatter(x_patch, y_patch)
+
+std_tar = 1500;
+num_tar = 50;
+tar_x_rec = [];
+tar_y_rec = []; % record target distribution
+for i = 1:num_patches
+    x_tar = x_patch(i) + std_tar * randn(num_tar, 1);
+    y_tar = y_patch(i) + std_tar * randn(num_tar, 1);
+    scatter(x_tar, y_tar)
+    hold on
+    tar_x_rec = [tar_x_rec; x_tar];
+    tar_y_rec = [tar_y_rec; y_tar];
+end
+
+data = [tar_x_rec, tar_y_rec];
+filename = 'target_3.csv';  % Name of the CSV file
+csvwrite(filename, data);
+save('TarDist3.mat','tar_x_rec','tar_y_rec');
+%%
+load("TarDistLarge.mat")
+scatter(tar_x_rec, tar_y_rec)
+xlb = xlabel('$x$ (m)','interpreter','Latex');
+ylb = ylabel('$y$ (m)','interpreter','Latex');
+set(gca,'TickLabelInterpreter','latex','fontsize',14);
+set([xlb,ylb],'interpreter','Latex','fontsize',18);
+saveas(gcf,'TargetDistribution.png')
